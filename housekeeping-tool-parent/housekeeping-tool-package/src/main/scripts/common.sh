d2ec56ba65c1c2fd0f40f876291a6093913a19fb
Courtney Edwards <cedwards@hotels.com>
2018-04-09 15:10:45 +0100

    Resolve HOUSEKEEPING_HOME in script

--- housekeeping-tool-parent/housekeeping-tool-package/src/main/scripts/common.sh
+++ housekeeping-tool-parent/housekeeping-tool-package/src/main/scripts/common.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-
-if [ -z ${CIRCUS_TRAIN_HOME-} ] && [ -d "/opt/circus-train" ] ; then
-  export CIRCUS_TRAIN_HOME=/opt/circus-train
+if [[ -z $HOUSEKEEPING_TOOL_HOME ]]; then
+  #work out the script location
+  SOURCE="${BASH_SOURCE[0]}"
+  while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$SCRIPT_DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  done
+  HOUSEKEEPING_TOOL_HOME="$( cd -P "$( dirname "$SOURCE" )" && cd .. && pwd )"
 fi
 
-if [[ -z ${CIRCUS_TRAIN_HOME-} ]]; then
-  echo "Set CIRCUS_TRAIN_HOME then run Tool"
-  exit
-fi
-
-echo "Using Circus Train Home $CIRCUS_TRAIN_HOME"
+echo "Using Housekeeping Home $HOUSEKEEPING_TOOL_HOME"
 
 if [[ -z ${HIVE_LIB-} ]]; then
   export HIVE_LIB=/usr/hdp/current/hive-client/lib
@@ -24,16 +25,16 @@ fi
 
 LIBFB303_JAR=`ls $HIVE_LIB/libfb303-*.jar | tr '\n' ':'`
 
-CIRCUS_TRAIN_LIBS=$CIRCUS_TRAIN_HOME/lib/*:$HIVE_LIB/hive-exec.jar:$HIVE_LIB/hive-metastore.jar:$LIBFB303_JAR:$HIVE_CONF_PATH
+HOUSEKEEPING_LIBS=$HOUSEKEEPING_TOOL_HOME/lib/*:$HIVE_LIB/hive-exec.jar:$HIVE_LIB/hive-metastore.jar:$LIBFB303_JAR:$HIVE_CONF_PATH
 
-if [[ -z ${CIRCUS_TRAIN_CLASSPATH-} ]]; then
-  export CIRCUS_TRAIN_CLASSPATH=$CIRCUS_TRAIN_LIBS
+if [[ -z ${HOUSEKEEPING_CLASSPATH-} ]]; then
+  export HOUSEKEEPING_CLASSPATH=$HOUSEKEEPING_LIBS
 else
-  export CIRCUS_TRAIN_CLASSPATH=$CIRCUS_TRAIN_CLASSPATH:$CIRCUS_TRAIN_LIBS
+  export HOUSEKEEPING_CLASSPATH=$HOUSEKEEPING_CLASSPATH:$HOUSEKEEPING_LIBS
 fi
 
 if [[ -z ${HADOOP_CLASSPATH-} ]]; then
-  export HADOOP_CLASSPATH=$CIRCUS_TRAIN_CLASSPATH
+  export HADOOP_CLASSPATH=$HOUSEKEEPING_CLASSPATH
 else
-  export HADOOP_CLASSPATH=$CIRCUS_TRAIN_CLASSPATH:$HADOOP_CLASSPATH
+  export HADOOP_CLASSPATH=$HOUSEKEEPING_CLASSPATH:$HADOOP_CLASSPATH
 fi
