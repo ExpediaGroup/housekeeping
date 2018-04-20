e261a2580609c5a81e35acc942643d1c76427416
Courtney Edwards <cedwards@hotels.com>
2018-04-20 15:23:50 +0100

    Format

--- src/main/java/com/hotels/housekeeping/service/impl/FileSystemHousekeepingService.java
+++ src/main/java/com/hotels/housekeeping/service/impl/FileSystemHousekeepingService.java
@@ -20,7 +20,6 @@ import static java.lang.String.format;
 import java.io.IOException;
 import java.util.List;
 
-import com.google.common.annotations.VisibleForTesting;
 import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
