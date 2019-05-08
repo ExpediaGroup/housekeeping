fdeae3f552b0d6f679a64d5803a5f5427f3c2158
Ana Jalba <ana.jalba13@gmail.com>
2019-05-08 16:24:40 +0100

    Cannot upgrade core to 5.4.2 because spring uses 5.1.0

--- housekeeping/src/main/java/com/hotels/housekeeping/audit/HousekeepingAuditRevision.java
+++ housekeeping/src/main/java/com/hotels/housekeeping/audit/HousekeepingAuditRevision.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2016-2018 Expedia Inc.
+ * Copyright (C) 2016-2019 Expedia Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,8 @@
  */
 package com.hotels.housekeeping.audit;
 
-import javax.persistence.Entity;
+import
+    javax.persistence.Entity;
 import javax.persistence.Table;
 
 import org.hibernate.envers.RevisionEntity;
